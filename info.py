8bb594dcbc57c559e401ecc1275fb9291e3f773a
iakshaytayade <akshaytayade525@gmail.com>
2025-03-03 07:28:19 +0530

    Update info.py
    Signed-off-by: iakshaytayade <akshaytayade525@gmail.com>

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID', '26518045')
-API_HASH = environ['API_HASH', 'e759fbf7d84113e47b18af5b665ee945')
-BOT_TOKEN = environ['BOT_TOKEN', "7593107061:AAE_bDL8t5r19w4Su8Wq9QUtAZ_evV-0BZo")
+API_ID = int(environ.get('API_ID', '26518045'))
+API_HASH = environ.get('API_HASH', 'e759fbf7d84113e47b18af5b665ee945')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7593107061:AAE_bDL8t5r19w4Su8Wq9QUtAZ_evV-0BZo")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
